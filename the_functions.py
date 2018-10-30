41e7027f350c39aac610faa382a3280479ca3175
简之易安 <44541840+jzya0828@users.noreply.github.com>
2018-10-30 22:34:03 +0800

    Revert "Merge branch 'master' of https://github.com/jzya0828/Python_self_leearning"
    This reverts commit 3f45d970ff409bd418853a96679369d399127134, reversing
    changes made to 65437d5959ff934ebed4586e0b36eb0a91d5dad0.

--- the_functions.py
+++ the_functions.py
@@ -1,52 +1,52 @@
 #-------functions----
 
-example1:write a function to exchange x1,x2
-def exchange(x1,x2):
-    print('x1 is ',x1,' x2 is ',x2)
-    if x1 != x2:
-        x1,x2=x2,x1
-    print('after exchange x1 is ',x1,' x2 is ',x2)
+#example1:write a function to exchange x1,x2
+# def exchange(x1,x2):
+#     print('x1 is ',x1,' x2 is ',x2)
+#     if x1 != x2:
+#         x1,x2=x2,x1
+#     print('after exchange x1 is ',x1,' x2 is ',x2)
+#
+# def main():
+#     exchange(2,7)
+#     exchange('s','c')
+#
+# main()#called main() method
 
-def main():
-    exchange(2,7)
-    exchange('s','c')
+# def getGrade(score):
+#     if score >= 90:
+#         return 'a'
+#     else:
+#         return 'b'
+#
+# def main():
+#     score = eval(input('input score'))
+#     print('the grade is ',getGrade(score))
+# main()
 
-main()#called main() method
+# def printGrade(score):
+#     if score < 0 or score > 100:
+#         print('invalid sore')
+#         return#return None
+#
+#     if score > 90:
+#         print('a')
+#     else:
+#         print('b')
+#
+#
+# score = 98
+# print('you score is',printGrade(score))
 
-def getGrade(score):
-    if score >= 90:
-        return 'a'
-    else:
-        return 'b'
-
-def main():
-    score = eval(input('input score'))
-    print('the grade is ',getGrade(score))
-main()
-
-def printGrade(score):
-    if score < 0 or score > 100:
-        print('invalid sore')
-        return#return None
-
-    if score > 90:
-        print('a')
-    else:
-        print('b')
-
-
-score = 98
-print('you score is',printGrade(score))
-
-#positional and keyword arguements
-def enroll(name,score,adress='beijing'):
-    print('name',name)
-    print('score',score)
-    print('adress',adress)
-
-enroll('sxl',90)
-enroll('sxl1',78,adress='xian')
-#the positional arguement cannot after any keywords arguements
+##positional and keyword arguements
+# def enroll(name,score,adress='beijing'):
+#     print('name',name)
+#     print('score',score)
+#     print('adress',adress)
+#
+# enroll('sxl',90)
+# enroll('sxl1',78,adress='xian')
+# #the positional arguement cannot after any keywords arguements
 
 # #Default Arguments
 # def printArea(width = 1,height = 2):
