8abd01d51d6fbb00b0591fa7d638449f78458716
简之易安 <44541840+jzya0828@users.noreply.github.com>
2018-11-02 20:48:29 +0800

    Update More ON Strings AND Specila Methods.py

---
+++ More ON Strings AND Specila Methods.py
@@ -0,0 +1,26 @@
+##---create string--
+
+# S = 'Welcome' # equaliy with S = str('Welcome')
+# print(len(S))#len()
+# print(max(S))#max()
+# print(min(S))#min()
+#
+# s = input('please enter a string')
+# if len(s) % 2 == 0:
+#     print(s,'contain an even number of character')
+# else:
+#     print(s,'contain an odd number of character')
+
+##--index Opeartor[]---
+
+# s = 'Welcome'
+# for i in range(0,len(s),2):
+#     print(s[i],end = '')
+
+# s1 = 'Welcome'
+# print(s1[-1])#s1[-1] is the same as s1[-1+len(s1)]
+# print(s1[-2])#s1[-2] is the same as s1[-2+len(s1)]
+
+#--the Slicing Operator [start:end]
+# s3 = 'Welcime'
+# print(s3[1 : 4])#3s[1 : 4] returns a substring from index i to 4-1
